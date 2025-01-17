5921acf8e586da4da56b661f8e66c515c8a98f5b
edwin08torres <at2899743@gmail.com>
2025-01-16 20:26:57 -0600

    t-dev: fix error de cors

--- my-inventory-app/vite.config.js
+++ my-inventory-app/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // redirige /api hacia MockAPI
+      '/api': {
+        target: 'https://67890cb02c874e66b7d76623.mockapi.io/Api/V1/',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
